b354db21c86e39dee5c3600086ac3ea21b9bfa98
中村俊 <shun@nakamurashunnoMacBook-Pro.local>
2019-06-28 21:24:22 +0900

    make create_function

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,4 +2,11 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
+
+  # 追記する
+  def create
+    Blog.create(title: params[:blog][:title], content: params[:blog][:content])
+    redirect_to new_blog_path
+  end
+
 end
